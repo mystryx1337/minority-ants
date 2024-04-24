b958408f412faf148a3c9ff73a8344b6b77c613b
mystryx1337 <mystryx@gmail.com>
2024-04-24 17:06:34 +0200

    prepared more ant code

--- minority_ant.py
+++ minority_ant.py
@@ -1,5 +1,5 @@
 import routing_ant
 
 class Minority_Ant(routing_ant.Routing_Ant):
-    def probability_for_node(self,target_node):
-        super().probability_for_node()
+    def _probability_for_node(self, target_node):
+        super()._probability_for_node()
--- routing_ant.py
+++ routing_ant.py
@@ -1,29 +1,35 @@
 import networkx as nx
 import random
+from typing import List
 
 class Routing_Ant:
-    G: nx.DiGraph
-    start_node: str
-    current_node: str
+    G: nx.DiGraph           # the graph, on which the ant is walking
+    start_node: str         # start node on the graph
+    current_node: str       # current node
 
-    alpha: float    #how much influence trail has
-    beta: float     #how much influence attractiveness has
+    alpha: float            # how much influence trail has
+    beta: float             # how much influence attractiveness has
 
-    success: bool   #does the ant put pheromones on its way
+    success: bool           # does the ant put pheromones on its way
 
-    max_steps: int  #how many steps can the ant do before giving up
-    steps: int
+    max_steps: int          # how many steps can the ant do before giving up
+    path: List[str]         # Path taken by the ant so far
+    path_cost: float = 0.0  # Cost of the path taken by the ant so far
 
-    def __init__(self, G, start_node, alpha=1, beta=1, max_steps=20):
+    def __init__(self, G: nx.DiGraph, start_node: str, alpha=1, beta=1, max_steps=20):
+        # set Parameters
         self.G = G
-        self.start_node = start_node
-        self.current_node = start_node
         self.alpha = alpha
         self.beta = beta
-        self.success = False
         self.max_steps = max_steps
+        self.start_node = start_node
 
-    def probability_for_node(self,target_node):
+        # Spawn
+        self.current_node = start_node
+        self.path.append(start_node)
+        self.success = False
+
+    def _probability_for_node(self, target_node: str) -> float:
         data = self.G[self.current_node][target_node]
         alpha = self.alpha
         beta = self.beta
@@ -34,7 +40,7 @@ class Routing_Ant:
 
         return ( (tau ** alpha) * (eta ** beta) ) / allowed_nodes_sum
 
-    def pick_a_new_node(self):
+    def _pick_a_new_node(self) -> str:
         possible_edges = self.G.edges([self.current_node], data=True)
         target_nodes = []
         probabilities = []
@@ -42,28 +48,41 @@ class Routing_Ant:
             pheromone = data["pheromone"]
             target_nodes.append(possible_target)
 
-            probability_for_node = self.probability_for_node(possible_target)
+            probability_for_node = self._probability_for_node(possible_target)
             probabilities.append(probability_for_node)
 
         return random.choices(target_nodes, weights=probabilities, k=1)[0]
 
-    def increase_pheromone(self,new_node):
+    def _get_unvisited_neighbors(self) -> List[str]:
+        possible_edges = self.G.edges([self.current_node], data=True)
+        unvisited_neighbors = []
+        for current_node, possible_target, data in possible_edges:
+            if possible_target not in self.path:
+                unvisited_neighbors.append(possible_target)
+        return unvisited_neighbors
+
+    def _check_success(self):
+        if self.G.nodes[self.current_node]['value'] > 0:
+            self.success = True
+
+    def _increase_pheromone(self, new_node):
         current_pheromones = self.G[self.current_node][new_node]['pheromone']
         self.G[self.current_node][new_node].update({'pheromone': current_pheromones + 1})
 
-    def step(self):
-        new_node = self.pick_a_new_node()
+    def _step(self):
+        new_node = self._pick_a_new_node()
+        self.path.append(new_node)
 
+        self._check_success()
+
+        #If on a Path for Success:
         if self.success:
-            self.increase_pheromone()
+            self._increase_pheromone(new_node)
 
         self.current_node = new_node
 
     def run(self):
-        self.steps = 0
-
         #Do steps, till it is successful and back
-        while self.current_node != self.start_node and not self.success and self.steps < self.max_steps:
-            self.step()
-            self.step += 1
+        while self.current_node != self.start_node and not self.success and len(self.path) < self.max_steps:
+            self._step()
 
