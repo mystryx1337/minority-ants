1cc4498b2d585093541b39b26ad27d112690d406
mystryx1337 <mystryx@gmail.com>
2024-05-03 11:12:40 +0200

    implemented minority ant

--- minority_ant.py
+++ minority_ant.py
@@ -8,6 +8,17 @@ class Minority_Ant(routing_ant.Routing_Ant):
         return super()._value_for_node(target_node)
 
     def _pick_a_new_node(self) -> str:
-        node_values = self._get_all_possible_nodes_value()
-        node_probabilities = np.divide(node_values,np.sum(node_values))
-        return random.choices(target_nodes, weights=node_probabilities, k=1)[0]
+        # random by chance
+        if random.random() < self.random_chance:
+            return super(routing_ant.Routing_Ant,self)._pick_a_new_node()
+
+        # pheromone sensitive behaviour, if pheromones on its way
+        node_values, target_nodes = self._get_all_unvisited_nodes_value()
+        sum_node_values = np.sum(node_values)
+        if(sum_node_values > 0):
+            node_probabilities = (node_values / sum_node_values)
+            node_probabilities = 1 - node_probabilities
+            return random.choices(target_nodes, weights=node_probabilities, k=1)[0]
+
+        # random otherwise
+        return super(routing_ant.Routing_Ant,self)._pick_a_new_node()
